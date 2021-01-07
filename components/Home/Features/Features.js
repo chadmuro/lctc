9ef524677ecae5781a194aae9e0e6b2b917760d7
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-07 15:18:40 +0900

    animation for ffeature cards on home page

--- components/Home/Features/Features.js
+++ components/Home/Features/Features.js
@@ -1,35 +1,72 @@
+import { useEffect } from 'react';
 import { IconContext } from 'react-icons';
 import { FcCommandLine, FcReading, FcConferenceCall } from 'react-icons/fc';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import styles from './Features.module.scss';
 
+const cardVariants = {
+	hidden: { scale: 0 },
+	visible: {
+		scale: 1,
+		transition: { duration: 0.3 },
+	},
+};
+
+const Card = ({ title, description, icon }) => {
+    const controls = useAnimation();
+		const [ref, inView] = useInView();
+
+		useEffect(() => {
+			if (inView) {
+				controls.start('visible');
+			}
+			if (!inView) {
+				controls.start('hidden');
+			}
+        }, [controls, inView]);
+
+        
+    return (
+			<motion.div
+				ref={ref}
+				initial="hidden"
+				animate={controls}
+				variants={cardVariants}
+				className={styles.features__card}
+			>
+				{icon}
+				<h3>{title}</h3>
+				<p>
+					{description}
+				</p>
+			</motion.div>
+		);
+}
+
 const Features = () => {
+	
 	return (
 		<IconContext.Provider value={{ className: styles.icon }}>
 			<div className={styles.features}>
-				<div className={styles.features__card}>
-					<FcCommandLine />
-					<h3>Projects</h3>
-					<p>
-						Build responsive web pages and web applications with our project
-						tutorials
-					</p>
-				</div>
-				<div className={styles.features__card}>
-					<FcReading />
-					<h3>Articles</h3>
-					<p>
-						Keep up with web development trends and learn new topics through our
-						weekly articles
-					</p>
-				</div>
-				<div className={styles.features__card}>
-					<FcConferenceCall />
-					<h3>Community</h3>
-					<p>
-						Join our community of learners; Learn and teach each other,
-						collaborate on projects, share ideas
-					</p>
-				</div>
+				<Card
+					title="Projects"
+					description="Build responsive web pages and web applications with our project
+                        tutorials"
+					icon={<FcCommandLine />}
+				/>
+				<Card
+					title="Articles"
+					description="Keep up with web development trends and learn new topics through our
+                        weekly articles"
+					icon={<FcReading />}
+				/>
+				<Card
+					title="Community"
+					description="Join our community of learners; Learn and teach each other,
+                        collaborate on projects, share ideas"
+					icon={<FcConferenceCall />}
+				/>
 			</div>
 		</IconContext.Provider>
 	);
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.scss';
 import Layout from '../components/Layout/Layout';
-import MediumContextProvider from '../contexts/MediumContext';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<MediumContextProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</MediumContextProvider>
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
 	);
 }
 
