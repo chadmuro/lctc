cb4a58acc1e20bb056d9b179fdb2305250704ca4
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-14 16:55:34 +0900

    create sanity studio

---
+++ client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+	projectId: 'lutq4u02',
+	dataset: 'production',
+});
--- pages/resources/index.js
+++ pages/resources/index.js
@@ -1,14 +1,55 @@
 import PageHero from '../../components/Pages/PageHero/PageHero';
+import styles from './styles.module.scss';
+
+const platforms = [
+	{
+		name: 'Youtube',
+		link: 'https://www.youtube.com/',
+	},
+	{
+		name: 'Codecademy',
+		link: 'https://www.codecademy.com/',
+	},
+	{
+		name: 'Udemy',
+		link: 'https://www.udemy.com/',
+	},
+	{
+		name: 'freeCodeCamp',
+		link: 'https://www.freecodecamp.org/',
+	},
+];
 
 const Resources = () => {
+    const openNewTab = url => {
+			const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
+			if (newWindow) newWindow.opener = null;
+		};
+
 	return (
 		<div className="resources">
 			<PageHero title="Resources" />
-			<p
-				style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}
-			>
-				Resources coming soon...
-			</p>
+			<main>
+				<div className={styles.content}>
+					<h3 className={styles.content__title}>Learning Platforms</h3>
+					<ul>
+						{platforms &&
+							platforms.map(platform => (
+								<li>
+									<p className={styles.content__name}>
+										{platform.name} &nbsp;
+										<a
+											className={styles.content__link}
+                                            onClick={() => openNewTab(platform.link)}
+										>
+											{platform.link}
+										</a>
+									</p>
+								</li>
+							))}
+					</ul>
+				</div>
+			</main>
 		</div>
 	);
 };
