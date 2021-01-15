829bf4d94f065698db6ee68d2716cd3d5f225ee6
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-15 17:12:24 +0900

    setup schemas and styling for resources

--- lctcstudio/schemas/schema.js
+++ lctcstudio/schemas/schema.js
@@ -6,11 +6,11 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
 import post from './post'
 import author from './author'
 import platform from './platform'
 import challenge from './challenge'
+import resource from './resource'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -23,9 +23,9 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    category,
     platform,
     challenge,
+    resource,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
--- pages/resources/index.js
+++ pages/resources/index.js
@@ -2,25 +2,6 @@ import PageHero from '../../components/Pages/PageHero/PageHero';
 import styles from './styles.module.scss';
 import sanityClient from '../../client';
 
-const platforms = [
-	{
-		name: 'Youtube',
-		link: 'https://www.youtube.com/',
-	},
-	{
-		name: 'Codecademy',
-		link: 'https://www.codecademy.com/',
-	},
-	{
-		name: 'Udemy',
-		link: 'https://www.udemy.com/',
-	},
-	{
-		name: 'freeCodeCamp',
-		link: 'https://www.freecodecamp.org/',
-	},
-];
-
 export async function getStaticProps() {
 	const platforms = await sanityClient.fetch(`*[_type == "platform"]{
         name,
@@ -35,22 +16,22 @@ export async function getStaticProps() {
 
 	return {
 		props: {
-            platforms,
-            challenges
+			platforms,
+			challenges,
 		},
 	};
 }
 
 const Resources = ({ platforms, challenges }) => {
-    const openNewTab = url => {
-			const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
-			if (newWindow) newWindow.opener = null;
-        };
+	const openNewTab = url => {
+		const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
+		if (newWindow) newWindow.opener = null;
+	};
 
 	return (
 		<div className="resources">
 			<PageHero title="Resources" />
-			<main>
+			<main className={styles.main}>
 				<div className={styles.content}>
 					<h3 className={styles.content__title}>Learning Platforms</h3>
 					<ul>
@@ -58,29 +39,29 @@ const Resources = ({ platforms, challenges }) => {
 							platforms.map(platform => (
 								<li key={platform._id}>
 									<p className={styles.content__name}>
-										{platform.name} &nbsp;
 										<a
 											className={styles.content__link}
-                                            onClick={() => openNewTab(platform.link)}
+											onClick={() => openNewTab(platform.link)}
 										>
-											{platform.link}
+											{platform.name}
 										</a>
 									</p>
 								</li>
 							))}
 					</ul>
+				</div>
+				<div className={styles.content}>
 					<h3 className={styles.content__title}>Coding Challenges</h3>
 					<ul>
 						{challenges &&
 							challenges.map(challenge => (
 								<li key={challenge._id}>
 									<p className={styles.content__name}>
-										{challenge.name} &nbsp;
 										<a
 											className={styles.content__link}
-                                            onClick={() => openNewTab(challenge.link)}
+											onClick={() => openNewTab(challenge.link)}
 										>
-											{challenge.link}
+											{challenge.name}
 										</a>
 									</p>
 								</li>
