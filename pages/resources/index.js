8c8915864a8ee23004a4b23b2ce85ff53a275e8c
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-15 19:00:22 +0900

    refractor resources index

--- pages/resources/index.js
+++ pages/resources/index.js
@@ -1,11 +1,21 @@
+import { motion } from 'framer-motion';
 import PageHero from '../../components/Pages/PageHero/PageHero';
 import styles from './styles.module.scss';
 import sanityClient from '../../client';
 
 export async function getStaticProps() {
-    const platforms = [];
-    const challenges = [];
-	const links = await sanityClient.fetch(`*[_type == "link"]{
+	const resources = [
+		{
+			title: 'Learning Platforms',
+			links: [],
+		},
+		{
+			title: 'Coding Challenges',
+			links: [],
+		},
+	];
+
+	const data = await sanityClient.fetch(`*[_type == "link"]{
         name,
         link,
         _id,
@@ -13,71 +23,69 @@ export async function getStaticProps() {
         'category': category->title
     }`);
 
-    links.forEach(link => {
-        if (link.category === 'Platform') {
-            platforms.push(link);
-        }
-        if (link.category === 'Challenge') {
-            challenges.push(link);
-        }
-    })
-	
+	data.forEach(link => {
+		if (link.category === 'Platform') {
+			resources[0].links.push(link);
+		}
+		if (link.category === 'Challenge') {
+			resources[1].links.push(link);
+		}
+	});
+
 	return {
 		props: {
-			platforms,
-			challenges,
+			resources
 		},
 	};
 }
 
-const Resources = ({ platforms, challenges }) => {
+const Resources = ({ resources }) => {
 	const openNewTab = url => {
 		const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
 		if (newWindow) newWindow.opener = null;
-    };
-    
-    console.log(platforms);
+	};
+
+	const linkVariants = {
+		hidden: { x: '-100vw' },
+		visible: {
+			x: 0,
+			transition: {
+				duration: 0.8,
+				type: 'spring',
+			},
+		},
+	};
 
 	return (
 		<div className="resources">
 			<PageHero title="Resources" />
 			<main className={styles.main}>
-				<div className={styles.content}>
-					<h3 className={styles.content__title}>Learning Platforms</h3>
-					<ul>
-						{platforms &&
-							platforms.map(platform => (
-								<li key={platform._id}>
-									<p className={styles.content__name}>
-										<a
-											className={styles.content__link}
-											onClick={() => openNewTab(platform.link)}
-										>
-											{platform.name}
-										</a>
-									</p>
-								</li>
-							))}
-					</ul>
-				</div>
-				<div className={styles.content}>
-					<h3 className={styles.content__title}>Coding Challenges</h3>
-					<ul>
-						{challenges &&
-							challenges.map(challenge => (
-								<li key={challenge._id}>
-									<p className={styles.content__name}>
-										<a
-											className={styles.content__link}
-											onClick={() => openNewTab(challenge.link)}
-										>
-											{challenge.name}
-										</a>
-									</p>
-								</li>
-							))}
-					</ul>
-				</div>
+				{resources &&
+					resources.map(resource => (
+						<div className={styles.content} key={resource.title}>
+							<h3 className={styles.content__title}>{resource.title}</h3>
+							<ul>
+								{resource.links &&
+									resource.links.map(link => (
+										<li key={link._id}>
+											<motion.p
+												className={styles.content__name}
+												initial="hidden"
+												animate="visible"
+												variants={linkVariants}
+											>
+												<a
+													className={styles.content__link}
+													onClick={() => openNewTab(link.link)}
+												>
+													{link.name}
+												</a>
+											</motion.p>
+										</li>
+									))}
+							</ul>
+						</div>
+					))}
 			</main>
 		</div>
 	);
