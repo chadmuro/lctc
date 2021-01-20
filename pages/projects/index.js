93e22ffa4fbfca473c75e7fb8134c885c1be3abd
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-20 14:44:39 +0900

    update projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,10 +4,17 @@ import React from 'react';
 
 const Projects = () => {
 	return (
-		<div>
+		<div className="projects">
 			<PageHero title="Projects" />
+			<p
+				style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}
+			>
+				New Projects coming soon...
+			</p>
 		</div>
 	);
 };
 
 export default Projects;
+
+
