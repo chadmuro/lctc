afefc599e0a5fb2754df1db3cdb67b65b18b1bde
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-02-01 14:36:39 +0900

    update icon hover animation

--- pages/mentor/index.js
+++ pages/mentor/index.js
@@ -4,6 +4,11 @@ import PageHero from '../../components/Pages/PageHero/PageHero';
 import styles from './styles.module.scss';
 
 const Mentor = () => {
+    const openNewTab = url => {
+			const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
+			if (newWindow) newWindow.opener = null;
+		};
+
 	return (
 		<div className={styles.mentor}>
 			<PageHero title="Mentor" />
@@ -17,8 +22,11 @@ const Mentor = () => {
 					<br />
 					<h4>
 						Message me on Twitter{' '}
-						<span className={styles.icon}>
-							<GrTwitter />
+						<span>
+							<GrTwitter
+								className={styles.icon}
+								onClick={() => openNewTab('https://twitter.com/chadmuro')}
+							/>
 						</span>{' '}
 						Let's have a chat!
 					</h4>
@@ -37,11 +45,13 @@ const Mentor = () => {
 						I know the struggle of learning to code and want to help others who
 						are starting out.
 					</p>
+                    <br />
 					<p>
 						I want to help others who decided they want to learn to code. I may
 						not have all the answers, but I would like to give you some advice,
 						resources, and motivation to help you get started.
 					</p>
+                    <br />
 					<p>
 						Learning to code is a tough journey, but you don't have to do it on
 						your own. Message me on Twitter and I would be happy to connect with
